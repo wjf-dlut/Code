ff465c4dfc7009d4b79f8402f056b10fc72a7846
your name <your@email.com>
2020-09-13 21:44:56 +0800

    test

--- test.cpp
+++ test.cpp
@@ -7,6 +7,9 @@ int main()
     int v1=0,v2=0;
     std::cin>>v1>>v2;
     std::cout<<"The sum of "<< v1 <<" and "<< v2 <<" is "<< v1+v2 <<std::endl;
-    system("pause");
+    // while(std::cin>>v2)
+    //     v1+=v2;
+    // std::cout<<"sum is "<<v1<<std::endl;
+    // system("pause");
     return 0;
 }
